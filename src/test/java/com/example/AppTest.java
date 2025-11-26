08ce1be09edc685fda969f0e9169b47db0afb064
root <root@localhost.localdomain>
2025-11-27 00:01:09 +0530

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testGreeting() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
